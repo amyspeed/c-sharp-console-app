469e937b0843691c395d62329dc492a1060657a5
Amy Speed <amy.speed.henley@gmail.com>
2020-08-12 01:28:06 -0500

    sort list

--- c-sharp-console-app/Program.cs
+++ c-sharp-console-app/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace c_sharp_console_app
 {
@@ -19,6 +20,52 @@ namespace c_sharp_console_app
             {
                 Console.WriteLine($"For loop i = {i}");
             }
+
+            // add all integers divisible by 3
+            int sum = 0;
+            for (int j = 0; j <= 20; j++)
+            {
+                if (j % 3 == 0)
+                {
+                    sum = sum + j;
+                }
+            }
+            Console.WriteLine($"the sum of this loop is {sum}");
+
+
+            // Arrays
+
+            var names = new List<string> { "name", "Ana", "Felipe" };
+            names.Add("Amy");
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+                //OR
+
+            for (int k = 0; k < names.Count; k++)
+            {
+                Console.WriteLine($"Hello {names[k].ToUpper()}!");
+            }
+
+            int index = names.IndexOf("Amy");
+
+            if (index == -1 )
+            {
+                Console.WriteLine("Can't find Amy!");
+            }
+            else
+            {
+                Console.WriteLine($"found {names[index]} at index {index}");
+            }
+
+            names.Sort();
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
         }
     }
 }
